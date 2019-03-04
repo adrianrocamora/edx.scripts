9f57ebc03a0893a8d50dbb942a8d43134eff62c8
Adrian Rocamora <adrianrocamorav@gmail.com>
2019-03-04 15:16:35 -0600

    Fixes to script

--- edx.install.sh
+++ edx.install.sh
@@ -3,7 +3,7 @@ cd ~/edx.scripts
 
 sudo chmod 755 edx.platform-bootstrap-ansible.sh
 sudo chmod 755 edx.platform-gen-pw.sh
-sudo chmod 755 edx.platform-onlyinstall.sh
+sudo chmod 755 edx.platform-only-install.sh
 
 sudo ./edx.platform-bootstrap-ansible.sh
 sudo ./edx.platform-gen-pw.sh
